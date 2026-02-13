bd86a2a55b3683f60dd0cd82217a00566e042196
pdsdahal <pdsdahal@gmail.com>
2026-02-13 10:27:57 -0600

    feat: Add GET product by ID and DELETE product - Added getProductById method in ProductService - Added deleteProduct method in ProductService - Added GET /products/{id} API in ProductController - Added DELETE /products/{id} API in ProductController

--- product-order-api/src/main/java/com/ecommerce/product_order_api/controller/ProductController.java
+++ product-order-api/src/main/java/com/ecommerce/product_order_api/controller/ProductController.java
@@ -2,7 +2,9 @@ package com.ecommerce.product_order_api.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,17 @@ public class ProductController {
 	@PostMapping
 	public Product addProduct(@RequestBody Product product) {
 		return productService.AddProduct(product);
+	}
+	
+	@GetMapping("/{id}")
+	public Product getProduct(@PathVariable Long id) {
+		return productService.getProduct(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public String deleteProduct(@PathVariable Long id) {
+		boolean result = productService.deleteProduct(id);
+		return result ? "Product deleted successfully!!!" : "Product not found"; 
 	}
 
 }
--- product-order-api/src/main/java/com/ecommerce/product_order_api/service/ProductService.java
+++ product-order-api/src/main/java/com/ecommerce/product_order_api/service/ProductService.java
@@ -11,19 +11,27 @@ import com.ecommerce.product_order_api.model.Product;
 //Holds business logic (in-memory for now)
 @Service
 public class ProductService {
-	
+
 	List<Product> productsList = new ArrayList<>();
 	AtomicLong idGenerator = new AtomicLong(1);
 
-	public List<Product> getAllProducts(){
+	public List<Product> getAllProducts() {
 		return productsList;
 	}
-	
-	
+
 	public Product AddProduct(Product product) {
 		product.setId(idGenerator.incrementAndGet());
 		productsList.add(product);
 		return product;
 	}
-	
+
+	public boolean deleteProduct(Long id) {
+		return productsList.removeIf(p -> p.getId().equals(id));
+	}
+
+	public Product getProduct(Long id) {
+		return productsList.stream().filter(p -> p.getId().equals(id))
+				.findFirst().orElse(null);
+	}
+
 }
