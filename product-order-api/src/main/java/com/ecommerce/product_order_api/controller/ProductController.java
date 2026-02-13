e6a4a3c5bf58caa37b52fd5aa6dcd7fb333fdece
pdsdahal <pdsdahal@gmail.com>
2026-02-13 11:58:09 -0600

    Add PUT product - Added updateProduct method in ProductService - Added updateProductById method in ProductController - Added  Initialize sample products method in ProductService

--- product-order-api/src/main/java/com/ecommerce/product_order_api/controller/ProductController.java
+++ product-order-api/src/main/java/com/ecommerce/product_order_api/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,31 +20,50 @@ import com.ecommerce.product_order_api.service.ProductService;
 @RequestMapping("/products")
 public class ProductController {
 
-	ProductService productService;
+	private final ProductService productService;
 
-	public ProductController() {
-		productService = new ProductService();
+	//Spring Boot manages object creation for you, 
+	//so you never have to use new for Spring-managed beans like services, repositories, etc.
+	public ProductController(ProductService productService) {
+		this.productService = productService;
 	}
 
+	// Day 1: Get all products
 	@GetMapping
 	public List<Product> getAllProducts() {
 		return productService.getAllProducts();
 	}
 
+	// Day 1: Add product
 	@PostMapping
 	public Product addProduct(@RequestBody Product product) {
-		return productService.AddProduct(product);
-	}
-	
-	@GetMapping("/{id}")
-	public Product getProduct(@PathVariable Long id) {
-		return productService.getProduct(id);
-	}
-	
-	@DeleteMapping("/{id}")
-	public String deleteProduct(@PathVariable Long id) {
-		boolean result = productService.deleteProduct(id);
-		return result ? "Product deleted successfully!!!" : "Product not found"; 
+		return productService.addProduct(product);
 	}
 
+	// Day 2: Get product by ID
+	@GetMapping("/{id}")
+	public Product getProductById(@PathVariable Long id) {
+		Product product = productService.getProductById(id);
+		if(product == null) {
+			throw new RuntimeException("Product not found");
+		}
+		return product;
+	}
+
+	// Day 2: Delete product by ID
+	@DeleteMapping("/{id}")
+	public String deleteProductById(@PathVariable Long id) {
+		boolean result = productService.deleteProduct(id);
+		return result ? "Product deleted successfully!!!" : "Product not found";
+	}
+
+	// Day 3: Update product
+	@PutMapping("/{id}")
+	public Product updateProductById(@PathVariable Long id, @RequestBody Product product) {
+		Product upProduct = productService.updateProduct(id, product);
+		if (upProduct == null) {
+			throw new RuntimeException("Product not found");
+		}
+		return upProduct;
+	}
 }
--- product-order-api/src/main/java/com/ecommerce/product_order_api/service/ProductService.java
+++ product-order-api/src/main/java/com/ecommerce/product_order_api/service/ProductService.java
@@ -4,34 +4,57 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 import com.ecommerce.product_order_api.model.Product;
 
 //Holds business logic (in-memory for now)
 @Service
-public class ProductService {
+public class ProductService implements CommandLineRunner {
+	
+	private final List<Product> productsList = new ArrayList<>();
+	private final AtomicLong idGenerator = new AtomicLong(1);
 
-	List<Product> productsList = new ArrayList<>();
-	AtomicLong idGenerator = new AtomicLong(1);
-
+	// Day 1: Get All Products
 	public List<Product> getAllProducts() {
 		return productsList;
 	}
 
-	public Product AddProduct(Product product) {
+	// Day 1: Add Product
+	public Product addProduct(Product product) {
 		product.setId(idGenerator.incrementAndGet());
 		productsList.add(product);
 		return product;
 	}
 
+	// Day 2: Delete Product
 	public boolean deleteProduct(Long id) {
 		return productsList.removeIf(p -> p.getId().equals(id));
 	}
 
-	public Product getProduct(Long id) {
-		return productsList.stream().filter(p -> p.getId().equals(id))
-				.findFirst().orElse(null);
+	// Day 2: Get Product by ID
+	public Product getProductById(Long id) {
+		return productsList.stream().filter(p -> p.getId().equals(id)).findFirst().orElse(null);
 	}
 
+	// Day 3: Update Product
+	public Product updateProduct(Long id, Product product) {
+		for (Product p : productsList) {
+			if (p.getId().equals(id)) {
+				p.setName(product.getName());
+				p.setPrice(product.getPrice());
+				p.setQuantity(product.getQuantity());
+				return p;
+			}
+		}
+		return null;
+	}
+
+	// Day 3: Initialize sample products
+	@Override
+	public void run(String... args) throws Exception {
+		addProduct(new Product(null, "iPhone", 999.99, 10));
+		addProduct(new Product(null, "Samsung Galaxy", 899.99, 8));
+	}
 }
