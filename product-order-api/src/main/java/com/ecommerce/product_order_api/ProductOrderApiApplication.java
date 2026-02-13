c73c187120bd4a02889f711d807cf6f613c12cef
pdsdahal <pdsdahal@gmail.com>
2026-02-12 22:22:24 -0600

    Initial Project SetUp

---
+++ product-order-api/src/main/java/com/ecommerce/product_order_api/ProductOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.product_order_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductOrderApiApplication.class, args);
+	}
+
+}
---
+++ product-order-api/src/test/java/com/ecommerce/product_order_api/ProductOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.product_order_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
